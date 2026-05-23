04ad9cd142dcc40c870481cf6e9fbe0b04cdc970
Kakashi-2009 <narutoxpanther@gmail.com>
2026-05-23 23:40:55 +0530

    fix vite config build error

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          charts: ['recharts'],
-          motion: ['framer-motion'],
-          math: ['mathjs'],
-        }
-      }
-    }
+    chunkSizeWarningLimit: 2000,
   }
 })
